b1c78b8cbac50496b08a234668b186e4fec5df6f
Shelby Sturgis <shelbyjsturgis@gmail.com>
2014-03-08 01:31:20 -0800

    adding to the Gruntfile and added .jshintrc file

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,17 @@ module.exports = function(grunt) {
     "use strict";
 
     grunt.initConfig({
+        concat: {
+            options: {
+                separator: ''
+            },
+            dist: {
+                src: [
+                    'js/ecuts/*.js'
+                ],
+                dest: 'js/ecuts.js'
+            }
+        },
         jshint: {
             options: {
                 jshintrc: '.jshintrc'
@@ -9,7 +20,8 @@ module.exports = function(grunt) {
             all: [
                 'Gruntfile.js',
                 'js/*.js',
-                'js/*.min.js'
+                'js/*.min.js',
+                'js/ecuts/*.js'
             ]
         },
         uglify: {
@@ -23,11 +35,55 @@ module.exports = function(grunt) {
                 }
             }
         },
-        watch: {
-            js: {
-
+        imagemin: {
+            dist: {
+                options: {
+                    optimizationLevel: 7,
+                    progressive: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'images/',
+                    src: '{,*/}*.{png,jpg,jpeg}',
+                    dest: 'images/'
+                }]
             }
+        },
+        watch: {
+            css: {
+                files: ['css/ecuts.css'],
+                tasks: ['cssmin']
+            },
+            js: {
+                files: [
+                    '<%= jshint.all %>'
+                ],
+                tasks: ['jshint', 'uglify']
+            }
+        },
+        cssmin: {
+            dist: {
+                files: {
+                    'css/ecuts.min.css' : ['css/ecuts.css']
+                }
+            }
+        },
+        clean: {
+            dist: [
+                'assets/css/ecuts.min.css',
+                'assets/js/ecuts.min.js'
+            ]
         }
-
     });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+    grunt.registerTask('default', ['concat', 'uglify', 'cssmin', 'imagemin', 'clean']);
+    grunt.registerTask('dev', ['watch']);
+    grunt.registerTask('lint', ['jshint']);
 };
